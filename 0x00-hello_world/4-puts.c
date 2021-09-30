568e1fecc6983c6dd1fd653285b93e2858a36523
cristhian1107 <cristhian.cjaa@gmail.com>
2021-09-30 18:49:30 +0000

    4 Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - put "\"Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 int
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
