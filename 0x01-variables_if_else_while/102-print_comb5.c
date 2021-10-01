2fc0d28693652aaf0cbe7a77892473682d5a8db9
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-01 21:34:14 +0000

    13 Task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,21 +8,32 @@ int main(void)
 {
 	int num1;
 	int num2;
+	int num3;
+	int num4;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = 0; num2 <= 8; num2++)
 		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
-			if (num1 == 9 && num2 == 9)
+			for (num3 = 0; num3 <= 9; num3++)
 			{
-				putchar('\n');
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
+				for(num4 = 0; num4 <= 9; num4++)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
