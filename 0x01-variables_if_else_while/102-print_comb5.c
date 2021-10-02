36ec3bfacc08570599aadc42654eb57e79ca0247
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-02 00:28:16 +0000

    porfin?! ahora si

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int num1;
 	int num2;
-	int num3;
-	int num4;
+	int num3 = 0;
+	int num4 = 0;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
@@ -17,11 +17,10 @@ int main(void)
 		{
 			for (num3 = num1; num3 <= 9; num3++)
 			{
-				num4 = num2 + 1;
-				if (num2 == 9)
-				{
+				if ((num2 == 8 && num4 == 0) || num2 == 0)
 					num4 = 0;
-				}
+				else
+					num4 = num2 + 1;
 				for (; num4 <= 9; num4++)
 				{
 					putchar(num1 + '0');
@@ -39,6 +38,7 @@ int main(void)
 						putchar(' ');
 					}
 				}
+				num4 = 0;
 			}
 		}
 	}
