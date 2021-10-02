c682055201b92810ef8d495dfe1d3ca78c2c08a7
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-02 03:23:50 +0000

    12./102-print_comb5./102-print_comb5./102-print_comb5! ya

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,44 +6,43 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4, temp1, temp2;
+int num1, num2, num3, num4, temp1, temp2;
 
-	num1 = num2 = num3 = num4 = 0;
-	while (num1 <= 9)
+num1 = num2 = num3 = num4 = 0;
+while (num1 <= 9)
+{
+	num2 = 0;
+	while (num2 <= 9)
 	{
-		num2 = 0;
-		while (num2 <= 9)
+		num3 = 0;
+		while (num3 <= 9)
 		{
-			num3 = 0;
-			while (num3 <= 9)
+			num4 = 0;
+			while (num4 <= 9)
 			{
-				num4 = 0;
-				while (num4 <= 9)
+				temp1 = (num1 * 10) + num2;
+				temp2 = (num3 * 10) + num4;
+				if (temp1 < temp2)
 				{
-					temp1 = (num1 * 10) + num2;
-					temp2 = (num3 * 10) + num4;
-					if (temp1 < temp2)
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
 					{
-						putchar(num1 + '0');
-						putchar(num2 + '0');
-						putchar(' ');
-						putchar(num3 + '0');
-						putchar(num4 + '0');
-						if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
-						{
-							putchar('\n');
-							break;
-						}
-						putchar(',');
-						putchar(' ');
+						putchar('\n');
 					}
-					num4++;
+					putchar(',');
+					putchar(' ');
 				}
-				num3++;
+				num4++;
 			}
-			num2++;
+			num3++;
 		}
-		num1++;
+		num2++;
 	}
-	return (0);
+	num1++;
+}
+return (0);
 }
