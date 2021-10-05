b29418ff406bfa4c7b4ea0a393feafbff5f178fb
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-05 14:24:34 +0000

    0 Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int _putchar(char);
+/**
+ * main - use _putchar
+ *
+ * Return: 0 int
+ */
+int main(void)
+{
+	/* Code ASCII "_putchar" */
+	int cdn[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, size;
+
+	/* 8 length */
+	size = sizeof(cdn) / sizeof(int);
+	for (count = 0; count < size; count++)
+	{
+		_putchar(cdn[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
