78dd65717f92fb344606e3d39590fb97a464745c
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-07 20:32:00 +0000

    3 Task - cambio de formula

--- 0x03-debugging/3-print_remaining_days-alt.c
+++ 0x03-debugging/3-print_remaining_days-alt.c
@@ -13,13 +13,12 @@
 void print_remaining_days(int month, int day, int year)
 {
 	/* Divisible entre 4, y no divisible entre 100 o divisible entre 400 */
-	if ((year % 4 == 0) && (year % 100 == 0 || year % 400 == 0))
+	if (year % 4 == 0 && (year % 100 == 0 || year % 400 == 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
