c5c9832d3e05c77bf7ba61a25b5486848054f974
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-07 17:56:05 +0000

    0 Task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - sign of the number
+ * @i: number
+ * Return: 0 int
+ */
+int positive_or_negative(int n)
+{
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
