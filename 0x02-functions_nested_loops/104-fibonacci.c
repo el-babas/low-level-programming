adae8ceb53b0ee1779a6a3d92d43cfdf7b52de77
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-07 00:55:31 +0000

    12 Task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+ * main - Fibonacci to 98
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int cont, isint, islong;
+	long int n1, n2, re;
+	long int nl1, nl2, rel;
+
+	n1 = n2 = re = 1;
+	cont = isint = islong = 1;
+	printf("%ld", re);
+	while (cont < 98)
+	{
+		if (isint == 1)
+		{
+			re = n1 + n2;
+			printf(", %ld", re);
+		}
+		else
+		{
+			if (islong == 1)
+			{
+				/*Dividimos el numero en dos parte*/
+				nl1 = n1 % 10000000000;
+				nl2 = n2 % 10000000000;
+				n1 = n1 / 10000000000;
+				n2 = n2 / 10000000000;
+				islong = 0;
+			}
+			rel = nl1 + nl2;
+			re = n1 + n2;
+			printf(", %ld%ld", (re + (rel / 10000000000)), (re % 10000000000));
+			nl1 = nl2;
+			nl2 = rel % 10000000000;
+		}
+		n1 = n2;
+		n2 = re;
+		cont++;
+		/*Verificamos si sale del limite de la variable*/
+		if (((n1 + n2) < 0) || islong == 0)
+			isint = 0;
+	}
+	printf("\n");
+	return (0);
+}
