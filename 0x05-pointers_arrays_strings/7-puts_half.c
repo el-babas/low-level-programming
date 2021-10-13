9e09269c80433f3efcff8ad1bcf002c8671485c6
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-13 01:54:44 +0000

    7-Task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - length
+ * @str: string
+ * Return: length string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int half = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		half = len / 2;
+	}
+	else
+	{
+		half = (len - 1) / 2;
+	}
+	for (; half < len; half++)
+	{
+		_putchar(*(str + half));
+	}
+	_putchar('\n');
+}
