1f3481a3c3b71d5cceb107d0a2656d557d6e0829
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-14 20:18:19 +0000

    10 Task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,29 +6,35 @@
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, dec = 1, pos = 0, num = 0, sign = 1, x;
+	unsigned int count = 0, dec = 0, pos = 0, num = 0, sign = 1, x;
 
 	while (*(s + count) != '\0')
 	{
 		/* Signos mas se anulan +*+=+ */
-		if (*(s + count) == '-' && dec == 1)
+		if (*(s + count) == '-' && dec == 0)
 			sign = -(sign);
 		/* Caracteres ASCII 0 - 9 */
 		if (*(s + count) >= 48 && *(s + count) <= 57)
 		{
-			dec *= 10;
-			if (dec == 10)
+			if (dec == 0)
+			{
+				dec = 1;
 				pos = count;
+			}
+			else
+			{
+				dec = dec * 10;
+			}
 		}
-		else if (dec >= 10)
+		else if (dec > 1)
 		{
 			break;
 		}
 		count++;
 	}
-	if (dec == 1)
+	if (dec == 0)
 		return (0);
-	for (x = 10; x <= dec; x *= 10)
+	for (x = 1; x <= dec; x *= 10)
 	{
 		num = num + ((*(s + pos) - 48) * (dec / x));
 		pos++;
