1f153447e0464d9b0ccf982940c3910cc4c14a1a
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-18 21:31:22 +0000

    4 Task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_memset(char *, char , unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
