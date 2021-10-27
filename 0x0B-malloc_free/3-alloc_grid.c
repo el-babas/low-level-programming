af2a9a7756d740eb5dd7a999a89ca00f2a392d53
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-27 01:44:25 +0000

    3 Task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - concatena cadena
+ * @width: cadena 1
+ * @height: cadena 2
+ * Return: int x * y
+ */
+int **alloc_grid(int width, int height)
+{
+	int x, a, b;
+	int **numbers;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	numbers = (int **)malloc(height * sizeof(int *));
+	if (numbers == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		*(numbers + x) = (int *)malloc(width * sizeof(int));
+		if (*(numbers + x) == NULL)
+			return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < height; b++)
+			numbers[a][b] = 0;
+	}
+	return (numbers);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,3 +2,4 @@ int _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int, int);
