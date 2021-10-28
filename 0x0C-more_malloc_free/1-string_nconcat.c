488e16affb9913efc65fda4d8eadc1a2387322cc
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-28 15:10:01 -0500

    1 Task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	s1 = (s1 == NULL) ? "" : s1;
 	s2 = (s2 == NULL) ? "" : s2;
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	len1 = strlen(s1) - 1;
+	len2 = strlen(s2) - 1;
 	if (len2 > n)
 		len2 = n;
 	len = len1 + len2;
