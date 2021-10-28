84a6dda8ef3f9a57e7704241e47a3fa6b17eef75
cristhian1107 <cristhian.cjaa@gmail.com>
2021-10-28 17:12:50 -0500

    2 Task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * _calloc - llena todo de cero
+ * @nmemb: longitud del contenido
+ * @size: longitud de tipo de dato byte
+ * Return: cadena
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int x;
+	char *str;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (x = 0; x < nmemb; x++)
+		*(str + x) = 0;
+	return (str);
+}
