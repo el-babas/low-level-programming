125bf2f8584aeb523cc1e4fd8e8169ba0c1f6a9d
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-03 22:40:29 -0500

    4 Task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - imprimir hexadecimal de bytes
+ * @argc: numero de argumentos
+ * @argv: argumentos
+ * Return: entero
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, x;
+	int (*file)(int, char **) = &main;
+	char op_code;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (x = 0; x < bytes; x++)
+	{
+		op_code = *(char *)(*file);
+		printf("%02x", op_code);
+		if (x != bytes - 1)
+			printf(" ");
+		(file)++;
+	}
+	printf("\n");
+	return (0);
+}
