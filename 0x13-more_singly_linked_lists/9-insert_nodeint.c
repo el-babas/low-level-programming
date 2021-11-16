ea729c427dd311dfdaed377b4bf8d224459c3f21
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-16 02:23:59 -0500

    9 Tasks Corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			tmp_head = tmp_head->next;
 			i++;
 		}
-		if (tmp_head == NULL && idx != i && idx != 0)
+		if (tmp_head == NULL && idx != 0)
 			return (NULL);
 		new_node = malloc(sizeof(listint_t));
 		new_node->n = n;
