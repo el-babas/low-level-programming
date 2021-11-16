94c4ab01cce96ef7468d025a661bc5345eb76649
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-16 02:34:56 -0500

    9 Tasks Corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
+	unsigned int i = 1;
 	listint_t *tmp_head, *new_node;
 
 	if (head != NULL)
