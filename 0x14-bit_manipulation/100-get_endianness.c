82a8ae226d79d44c9a14560ead40abac06f8fb32
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-18 21:53:22 -0500

    6 Task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+/**
+ * get_endianness - Write a function that checks the endianness.
+ */
+int get_endianness(void)
+{
+	int16_t i = 1;
+	int8_t *p = (int8_t *) &i;
+
+	if (p[0] == 1)
+		return (1);
+	else
+		return (0);
+}
