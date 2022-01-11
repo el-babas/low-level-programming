7e3b5c17ed9cf6a7df477c5e0dc4d38fbc21d429
cristhian1107 <cristhian.cjaa@gmail.com>
2022-01-10 19:53:57 -0500

    3 Task advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - example function
+ * @a: first number
+ * @b: second number
+ * Return: operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - example function
+ * @a: first number
+ * @b: second number
+ * Return: operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - example function
+ * @a: first number
+ * @b: second number
+ * Return: operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - example function
+ * @a: first number
+ * @b: second number
+ * Return: operation
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - example function
+ * @a: first number
+ * @b: second number
+ * Return: operation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
