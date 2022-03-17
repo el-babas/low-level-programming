c84a5505bafaa1ebb9792cc7333a0662b87c18ca
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-17 15:25:44 -0500

    5 Task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,12 +25,12 @@ def island_perimeter(grid):
     for x in range(0, rows):
         for y in range(0, len(grid[x])):
             if grid[x][y] == land:
-                if (y + 1) < len(grid[x]) and grid[x][y + 1] == water:
+                if (y + 1) == len(grid[x]) or grid[x][y + 1] == water:
                     result += 1
-                if (y - 1) != -1 and grid[x][y - 1] == water:
+                if (y - 1) == -1 or grid[x][y - 1] == water:
                     result += 1
-                if (x + 1) < rows and grid[x + 1][y] == water:
+                if (x + 1) == rows or grid[x + 1][y] == water:
                     result += 1
-                if (x - 1) != -1 and grid[x - 1][y] == water:
+                if (x - 1) == -1 or grid[x - 1][y] == water:
                     result += 1
     return result
