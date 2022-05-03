6337c3be0ade7a1de2c25ed2e0257016a68d0cd6
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-03 16:57:00 -0500

    Feat (Add) linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+
+/*==== Libraries ====*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*==== Prototype =====*/
+int linear_search(int *array, size_t size, int value);
+
+#endif
