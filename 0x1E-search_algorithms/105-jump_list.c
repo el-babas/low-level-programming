621a90fcb5588406cb84797709861ea279d79330
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-04 15:55:40 -0500

    Feat (Add) Jump search in Linked List

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Searches for a value in a sorted linked list
+ *	of integers using the Jump search algorithm.
+ *
+ * @list: The pointer to the first element of the list.
+ * @size: The size of the array.
+ * @value: The value to search.
+ *
+ * Return: The first index where value is located.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t root = 0, i = 0;
+	/* st = start and ed = end */
+	listint_t *st = list, *ed = list;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	root = sqrt(size);
+
+	while (ed->next != NULL && ed->n < value)
+	{
+		st = ed;
+		for (i = 0; i < root && ed->next != NULL; i++)
+			ed = ed->next;
+		printf("Value checked array[%ld] = [%d]\n", ed->index, ed->n);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", st->index, ed->index);
+	for ( ; st->index < size && st->index <= ed->index; st = st->next)
+	{
+		printf("Value checked array[%ld] = [%d]\n", st->index, st->n);
+		if (st->n == value)
+			return (st);
+	}
+
+	return (NULL);
+}
